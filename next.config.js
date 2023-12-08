68a9442095231a54bb18c21b0819ab55ebc3caf1
amar.s <sameeranamarnath@gmail.com>
2023-12-08 12:54:48 +0530

    moved styled components config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone' //docker compatibility
+  output: 'standalone', //docker compatibility
+  compiler:{
+    styledComponents:{ 
+    ssr:true,
+    displayName:true 
+    }
+  }
 }
+
 
 module.exports = nextConfig
